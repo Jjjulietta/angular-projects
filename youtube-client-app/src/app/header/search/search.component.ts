579ce8189484260a8ca366307c1451aaad36d1b0
Jjjulietta <jjjuliettai@gmail.com>
2023-10-23 10:39:49 +0300

    feat: implement search component

--- youtube-client-app/src/app/header/search/search.component.ts
+++ youtube-client-app/src/app/header/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent {
+  public inputText: string = '';
+  @Output() submit = new EventEmitter();
 
+  ngOnChange(text: string) {
+    this.inputText = text;
+    this.submit.emit(this.inputText);
+  }
 }
