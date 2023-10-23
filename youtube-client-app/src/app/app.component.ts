27c4f3f122e4d80ffeaa3fbb1b0a369902708409
Jjjulietta <jjjuliettai@gmail.com>
2023-10-23 23:52:20 +0300

    feat: implement functionality at the app level

--- youtube-client-app/src/app/app.component.ts
+++ youtube-client-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { data } from 'src/data/mock-data';
+import { SearchItem } from './models/search-item.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'youtube-client-app';
+  public shown = false;
+  public sort = '';
+  public sortView = '';
+  public word = '';
+  public submit = '';
+  public resultsSearch: SearchItem[] = [];
+
+  toggleFilter(shown: boolean) {
+    this.shown = shown;
+  }
+
+  toggleSort(sort: string) {
+    this.sort = sort;
+    console.log(this.sort);
+  }
+
+  toggleSortView(sortView: string) {
+    console.log(sortView);
+    this.sortView = sortView;
+    console.log(this.sortView);
+  }
+
+  inputWord(word: string) {
+    console.log(word);
+    this.word = word;
+  }
+
+  downloadResults(submit: string) {
+    if (submit) this.resultsSearch = data;
+  }
 }
--- youtube-client-app/src/app/header/header.component.ts
+++ youtube-client-app/src/app/header/header.component.ts
@@ -8,9 +8,15 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class HeaderComponent {
   public isShown = false;
   public inputSearch = '';
+  public sort = '';
+  public sortView = '';
+  public word = '';
 
   @Output() onClick = new EventEmitter();
   @Output() onSubmit = new EventEmitter();
+  @Output() onSortDate = new EventEmitter();
+  @Output() onSortView = new EventEmitter();
+  @Output() onFilterWord = new EventEmitter();
 
   toggleFilterBlock() {
     this.isShown = !this.isShown;
@@ -20,5 +26,24 @@ export class HeaderComponent {
   clickSearch(input: string) {
     this.inputSearch = input;
     this.onSubmit.emit(this.inputSearch);
+  }
+
+  toggleSort(sort: string) {
+    this.sort = sort;
+    console.log(this.sort);
+    this.onSortDate.emit(this.sort);
+  }
+
+  toggleSortView(sortView: string) {
+    console.log(sortView);
+    this.sortView = sortView;
+    console.log(this.sortView);
+    this.onSortView.emit(this.sortView);
+  }
+
+  inputWord(word: string) {
+    console.log(word);
+    this.word = word;
+    this.onFilterWord.emit(this.word);
   }
 }
