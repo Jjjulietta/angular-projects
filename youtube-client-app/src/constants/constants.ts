ec19464bdbeb0719b66d42035a0eb9e13c0f3c9e
Jjjulietta <jjjuliettai@gmail.com>
2023-10-23 10:57:14 +0300

    feat: add constants

---
+++ youtube-client-app/src/constants/constants.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  blue: '#2f80ed',
+  red: 'red',
+  green: 'green',
+  yellow: 'yellow',
+};
+
+export const period = {
+  sixMonth: 180,
+  month: 30,
+  sevenDay: 7,
+};
