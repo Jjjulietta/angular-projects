8bb1a98b645a6160bb8cf327cc8ed6ada4046c22
Jjjulietta <jjjuliettai@gmail.com>
2023-10-23 13:46:10 +0300

    feat: implement filter-block component

--- youtube-client-app/src/app/header/filter-block/filter-block.component.ts
+++ youtube-client-app/src/app/header/filter-block/filter-block.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-filter-block',
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./filter-block.component.scss'],
 })
 export class FilterBlockComponent {
+  public sort = 'default';
+  public sortView = 'default';
+  public inputWord = '';
+  @Output() onSortDate = new EventEmitter();
+  @Output() onSortView = new EventEmitter();
+  @Output() onFilterWord = new EventEmitter();
 
+  clickSortDate() {
+    if (this.sort === 'default' || this.sort === 'desc') {
+      this.sort = 'asc';
+    } else if (this.sort === 'asc') {
+      this.sort = 'desc';
+    }
+    console.log(this.sort);
+    this.onSortDate.emit(this.sort);
+  }
+
+  clickSortView() {
+    if (this.sortView === 'default' || this.sortView === 'desc') {
+      this.sortView = 'asc';
+    } else if (this.sortView === 'asc') {
+      this.sortView = 'desc';
+    }
+    console.log(this.sortView);
+    this.onSortView.emit(this.sortView);
+  }
+
+  ngOnChange(word: string) {
+    console.log(word);
+    this.onFilterWord.emit(word);
+  }
 }
