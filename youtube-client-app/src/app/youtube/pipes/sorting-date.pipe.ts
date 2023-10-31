482e13c92fdf64b94405426c9cf5f7aff61745ef
Jjjulietta <jjjuliettai@gmail.com>
2023-10-31 16:58:24 +0300

    fix: change the logic inside the sorting-date pipe

--- youtube-client-app/src/app/youtube/pipes/sorting-date.pipe.ts
+++ youtube-client-app/src/app/youtube/pipes/sorting-date.pipe.ts
@@ -7,18 +7,22 @@ import { SearchItem } from '../models/search-item.model';
 export class SortingDatePipe implements PipeTransform {
   transform(cards: SearchItem[], value: string): SearchItem[] {
     console.log(value);
-    if (value === 'asc') {
+
+    function sortCardsAsc() {
       return cards.sort(
         (a, b) =>
           new Date(a.snippet.publishedAt).getTime() -
           new Date(b.snippet.publishedAt).getTime()
       );
-    } else {
+    }
+
+    function sortCardsDesc() {
       return cards.sort(
         (a, b) =>
           new Date(b.snippet.publishedAt).getTime() -
           new Date(a.snippet.publishedAt).getTime()
       );
     }
+    return value === 'asc' ? sortCardsAsc() : sortCardsDesc();
   }
 }
