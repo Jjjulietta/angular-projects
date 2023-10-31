51c175cbe50b6b759c6114747131e5d1eca30ff3
Jjjulietta <jjjuliettai@gmail.com>
2023-10-31 14:29:57 +0300

    feat: add lazy loading for youtube module

--- youtube-client-app/src/app/app-routing.module.ts
+++ youtube-client-app/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchResultsBlockComponent } from './youtube/pages/search-results-block/search-results-block.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'main',
+    component: SearchResultsBlockComponent,
+    loadChildren: () =>
+      import('./youtube/youtube.module').then((m) => m.YoutubeModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
