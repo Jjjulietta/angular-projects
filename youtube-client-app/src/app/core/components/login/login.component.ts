41f4270dfb7baa91699640f17098bf9a7ae1acbb
Jjjulietta <jjjuliettai@gmail.com>
2023-11-03 20:02:22 +0300

    feat: add authentication check to login component

--- youtube-client-app/src/app/core/components/login/login.component.ts
+++ youtube-client-app/src/app/core/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  name: string = '';
+  constructor(private router: Router, private AuthService: AuthService) {
+    this.AuthService.name.subscribe((val) => (this.name = val));
+  }
 
+  ngOnInit() {
+    if (!this.AuthService.checkAuth()) {
+      this.router.navigate(['login']);
+    }
+    /*if (this.AuthService.checkAuth()) {
+      this.AuthService.name.subscribe((val) => (this.name = val));
+      console.log('init login');
+      this.router.navigate(['']);
+    } else {
+      this.router.navigate(['login']);
+    }*/
+  }
+
+  openLoginForm() {
+    console.log(this.AuthService.checkAuth());
+    if (this.AuthService.checkAuth()) this.AuthService.loggout();
+    this.router.navigate(['login']);
+  }
 }
