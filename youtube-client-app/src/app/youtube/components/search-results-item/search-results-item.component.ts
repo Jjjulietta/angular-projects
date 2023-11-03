b022838f378ac5e8c2d30ee6da8942b92c5a0978
Jjjulietta <jjjuliettai@gmail.com>
2023-11-03 10:38:15 +0300

    feat: add a method for opening the details page

--- youtube-client-app/src/app/youtube/components/search-results-item/search-results-item.component.ts
+++ youtube-client-app/src/app/youtube/components/search-results-item/search-results-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { SearchItem } from 'src/app/youtube/models/search-item.model';
 
 @Component({
@@ -10,7 +11,14 @@ export class SearchResultsItemComponent {
   @Input() card?: SearchItem;
 
   date?: string;
+
+  constructor(private router: Router) {}
   ngOnInit() {
     if (this.card) this.date = this.card?.snippet.publishedAt;
+  }
+
+  openCard() {
+    console.log(this.card?.id);
+    this.router.navigate([`detailed/${this.card?.id}`]);
   }
 }
