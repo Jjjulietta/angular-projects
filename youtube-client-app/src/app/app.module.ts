817c89fd2fad8cdbe014c7f7cd6ba582a7c96451
Jjjulietta <jjjuliettai@gmail.com>
2023-11-10 20:11:29 +0300

    feat: create http interceptor

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { CustomButtonComponent } from './custom-button/custom-button.component';
+import { ApiInterceptor } from './youtube/interceptors/api.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,7 +18,9 @@ import { CustomButtonComponent } from './custom-button/custom-button.component';
     CoreModule,
     ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ youtube-client-app/src/app/youtube/interceptors/api.interceptor.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class ApiInterceptor implements HttpInterceptor {
+  private URL = 'https://www.googleapis.com/youtube/v3';
+  constructor() {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    return next.handle(
+      request.clone({
+        url: `${this.URL}/${request.url}`,
+        //setParams: { key: API_KEY },
+      })
+    );
+  }
+}
