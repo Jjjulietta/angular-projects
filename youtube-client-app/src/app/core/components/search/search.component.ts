5fe90c08f254aa286b957efeeb766b1ff1478ac3
Jjjulietta <jjjuliettai@gmail.com>
2023-11-10 18:04:09 +0300

    feat: implement search without button

--- youtube-client-app/src/app/core/components/search/search.component.ts
+++ youtube-client-app/src/app/core/components/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Output, SimpleChanges } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 import { YoutubeService } from 'src/app/youtube/services/youtube.service';
 
 @Component({
@@ -9,14 +11,30 @@ import { YoutubeService } from 'src/app/youtube/services/youtube.service';
 })
 export class SearchComponent {
   public inputText: string | null = null;
+  searchForm: FormGroup = new FormGroup({
+    search: new FormControl(''),
+  });
   constructor(private router: Router, private YoutubeService: YoutubeService) {}
 
-  search(text: string) {
+  ngOnInit() {
+    this.searchForm.controls['search'].valueChanges.subscribe(
+      (val) => {
+        if (debounceTime(500) && val.length > 3 && distinctUntilChanged()) {
+          console.log(val);
+          this.YoutubeService.submit.next(val);
+        }
+      }
+
+      //tap((val) => console.log(val) /*this.YoutubeService.submit.next(val)*/)
+    );
+  }
+
+  /*ngOnChange(text: string) {
     this.inputText = text;
     if (this.inputText !== null) {
       this.YoutubeService.submit.next(this.inputText);
       console.log(this.inputText);
       this.router.navigate(['main']);
     }
-  }
+  }*/
 }
--- youtube-client-app/src/app/core/core.module.ts
+++ youtube-client-app/src/app/core/core.module.ts
@@ -6,6 +6,7 @@ import { SearchComponent } from './components/search/search.component';
 import { FilterBlockComponent } from './pages/filter-block/filter-block.component';
 import { CustomButtonComponent } from '../custom-button/custom-button.component';
 import { NotFoundPageComponent } from '../youtube/pages/not-found-page/not-found-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { NotFoundPageComponent } from '../youtube/pages/not-found-page/not-found
     SearchComponent,
     FilterBlockComponent,
   ],
-  imports: [CommonModule, CustomButtonComponent],
+  imports: [CommonModule, CustomButtonComponent, ReactiveFormsModule],
   exports: [HeaderComponent],
 })
 export class CoreModule {}
