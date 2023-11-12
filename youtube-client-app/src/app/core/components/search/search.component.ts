92b5f020f6ab04ad4723e677e43159a57361f79f
Jjjulietta <jjjuliettai@gmail.com>
2023-11-12 23:27:49 +0300

    fix: change search form

--- youtube-client-app/src/app/core/components/search/search.component.ts
+++ youtube-client-app/src/app/core/components/search/search.component.ts
@@ -1,7 +1,16 @@
 import { Component, EventEmitter, Output, SimpleChanges } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
-import { debounceTime, distinctUntilChanged } from 'rxjs';
+import {
+  BehaviorSubject,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  Observable,
+  Subject,
+  takeUntil,
+} from 'rxjs';
+import { SearchItemVideo } from 'src/app/youtube/models/search-item.model';
 import { YoutubeService } from 'src/app/youtube/services/youtube.service';
 
 @Component({
@@ -11,13 +20,15 @@ import { YoutubeService } from 'src/app/youtube/services/youtube.service';
 })
 export class SearchComponent {
   public inputText: string | null = null;
-  searchForm: FormGroup = new FormGroup({
+  search: string = '';
+  subscription$ = new Subject<void>();
+  /*searchForm: FormGroup = new FormGroup({
     search: new FormControl(''),
-  });
-  constructor(private router: Router, private YoutubeService: YoutubeService) {}
+  });*/
+  constructor(private router: Router, private youtubeService: YoutubeService) {}
 
   ngOnInit() {
-    this.searchForm.controls['search'].valueChanges.subscribe(
+    /*this.searchForm.controls['search'].valueChanges.subscribe(
       (val) => {
         if (debounceTime(600) && val.length > 3 && distinctUntilChanged()) {
           console.log(val);
@@ -25,10 +36,21 @@ export class SearchComponent {
         }
       }
 
-      //tap((val) => console.log(val) /*this.YoutubeService.submit.next(val)*/)
-    );
+      //tap((val) => console.log(val) /*this.YoutubeService.submit.next(val)*/
   }
 
+  onChange() {
+    if (debounceTime(600) && this.search.length > 3 && distinctUntilChanged()) {
+      console.log(this.search);
+      this.youtubeService
+        .search(this.search)
+        .pipe(takeUntil(this.subscription$))
+        .subscribe((val) => {
+          console.log(val);
+          this.youtubeService.resultsSearch.next(val);
+        });
+    }
+  }
   /*ngOnChange(text: string) {
     this.inputText = text;
     if (this.inputText !== null) {
@@ -37,4 +59,9 @@ export class SearchComponent {
       this.router.navigate(['main']);
     }
   }*/
+
+  ngOnDestroy() {
+    this.subscription$.next();
+    this.subscription$.complete();
+  }
 }
