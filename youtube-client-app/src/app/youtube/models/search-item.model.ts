92e48da8c6afc73b86eb9733486cf94f54034401
Jjjulietta <jjjuliettai@gmail.com>
2023-11-13 17:25:45 +0300

    feat: add date type to interface

--- youtube-client-app/src/app/youtube/models/search-item.model.ts
+++ youtube-client-app/src/app/youtube/models/search-item.model.ts
@@ -71,6 +71,6 @@ export interface SearchCards {
   img: string | null;
   link?: string | null;
   description: string | null;
-  date: string | null;
+  date: DateConstructor | string | null;
   statistics?: SearchItemStatistics;
 }
