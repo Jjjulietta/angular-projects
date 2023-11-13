6af9f71cc24ed6438f53661030328f3ca1d6d5de
Jjjulietta <jjjuliettai@gmail.com>
2023-11-13 11:28:53 +0300

    refactor: edit code

--- youtube-client-app/src/app/core/pages/header/header.component.ts
+++ youtube-client-app/src/app/core/pages/header/header.component.ts
@@ -7,43 +7,12 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent {
   public isShown = false;
-  // public inputSearch = '';
-  // public sort = '';
-  // public sortView = '';
   public word = '';
 
   @Output() onClick = new EventEmitter();
-  // @Output() onSubmit = new EventEmitter();
-  // @Output() onSortDate = new EventEmitter();
-  // @Output() onSortView = new EventEmitter();
-  // @Output() onFilterWord = new EventEmitter();
 
   toggleFilterBlock() {
     this.isShown = !this.isShown;
     this.onClick.emit(this.isShown);
   }
-
-  /*clickSearch(input: string) {
-    this.inputSearch = input;
-    this.onSubmit.emit(this.inputSearch);
-  }
-
-  toggleSort(sort: string) {
-    this.sort = sort;
-    console.log(this.sort);
-    this.onSortDate.emit(this.sort);
-  }
-
-  toggleSortView(sortView: string) {
-    console.log(sortView);
-    this.sortView = sortView;
-    console.log(this.sortView);
-    this.onSortView.emit(this.sortView);
-  }
-
-  inputWord(word: string) {
-    console.log(word);
-    this.word = word;
-    this.onFilterWord.emit(this.word);
-  } */
 }
