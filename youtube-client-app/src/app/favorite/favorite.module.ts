42c77dc5f120f412ab56235f01c69d9f7c88070e
Jjjulietta <jjjuliettai@gmail.com>
2023-11-16 10:48:22 +0300

    feat: implemet favotite page

--- youtube-client-app/src/app/favorite/favorite.module.ts
+++ youtube-client-app/src/app/favorite/favorite.module.ts
@@ -3,15 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { FavoriteRoutingModule } from './favorite-routing.module';
 import { FavoriteComponent } from './favorite.component';
-
+import { SearchResultsItemComponent } from '../youtube/components/search-results-item/search-results-item.component';
+import { YoutubeModule } from '../youtube/youtube.module';
 
 @NgModule({
-  declarations: [
-    FavoriteComponent
-  ],
-  imports: [
-    CommonModule,
-    FavoriteRoutingModule
-  ]
+  declarations: [FavoriteComponent],
+  imports: [CommonModule, FavoriteRoutingModule, YoutubeModule],
 })
-export class FavoriteModule { }
+export class FavoriteModule {}
