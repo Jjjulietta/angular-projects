34b75d6f9a3969cde73f625c813f9278ac28460c
Jjjulietta <jjjuliettai@gmail.com>
2023-11-20 14:02:15 +0300

    fix: change selectors

--- youtube-client-app/src/app/redux/selectors/cards.selector.ts
+++ youtube-client-app/src/app/redux/selectors/cards.selector.ts
@@ -1,22 +1,71 @@
+import { state } from '@angular/animations';
 import { createFeatureSelector, createSelector } from '@ngrx/store';
-import { AppState } from '../state.models';
+import { SearchCards } from 'src/app/youtube/models/search-item.model';
+import { AppState, YotubeResultsInterface } from '../state.models';
 
 export const selectYoutubeCards =
-  createFeatureSelector<Pick<AppState, 'youtubeCards'>>('youtubeCards');
+  createFeatureSelector<YotubeResultsInterface>('youtubeCards');
 export const selectCustomcards =
   createFeatureSelector<Pick<AppState, 'customCards'>>('customCards');
 export const selectCustomLength = createSelector(
   selectCustomcards,
   (s) => s.customCards.length
 );
-export const selectCards = createSelector(
-  selectCustomcards,
-  selectYoutubeCards,
-  (s1, s2) => {
-    return s1.customCards.concat(s2.youtubeCards);
-  }
+
+export const selectPage =
+  createFeatureSelector<Pick<AppState, 'cardsPage'>>('cardsPage');
+
+export const selectPageNumber = createSelector(
+  selectPage,
+  (s1) => s1.cardsPage
 );
 
+export const isLoadingSelector = createSelector(
+  selectYoutubeCards,
+  (state) => state.isLoading
+);
+
+export const selectLoadingPages = createSelector(selectYoutubeCards, (state) =>
+  Object.keys(state.cards)
+);
+
+export const errorSelector = createSelector(
+  selectYoutubeCards,
+  (state) => state.error
+);
+
+export const selectCards = createSelector(
+  selectYoutubeCards,
+  selectPage,
+  (s1, s2) => s1.cards[s2.cardsPage]
+);
+
+export const selectAllCards = createSelector(
+  selectCustomcards,
+  selectYoutubeCards,
+  selectPage,
+  (s1, s2, s3) => s1.customCards.concat(s2.cards[s3.cardsPage]) /*{
+    if (s3.cardsPage === '1') {
+      return s1.customCards.concat(s2.cards[s3.cardsPage]);
+    } else {
+      return s2.cards[s3.cardsPage];
+    }
+  }*/
+);
+
+/*{
+    if (s3.cardsPage === '1') {
+      return s1.customCards.concat(s2.cards[s3.cardsPage]);
+    } else {
+      return s2.cards[s3.cardsPage];
+    }
+  }*/
+
 export const selectFavoriteCards = createSelector(selectYoutubeCards, (s1) => {
-  return s1.youtubeCards.filter((item) => item.favorite === 'true');
+  return (
+    Object.values(s1.cards)
+      //.map((item) => ({ ...item }))
+      .map((item) => item.filter((item) => item.favorite === 'true'))
+      .flat()
+  );
 });
