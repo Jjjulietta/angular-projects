e44939d9840b363314237a823be9f8b65be7ec3d
Jjjulietta <jjjuliettai@gmail.com>
2023-11-20 13:59:18 +0300

    fix: change app state interface

--- youtube-client-app/src/app/redux/state.models.ts
+++ youtube-client-app/src/app/redux/state.models.ts
@@ -2,5 +2,16 @@ import { SearchCards } from '../youtube/models/search-item.model';
 
 export interface AppState {
   customCards: SearchCards[];
-  youtubeCards: SearchCards[];
+  youtubeCards: YotubeResultsInterface; //YoutubeResultsState; //SearchCards[];
+  cardsPage: string;
+}
+
+export interface YoutubeResultsState {
+  [key: string]: SearchCards[] | [];
+}
+
+export interface YotubeResultsInterface {
+  isLoading: boolean;
+  cards: YoutubeResultsState;
+  error: string | null;
 }
