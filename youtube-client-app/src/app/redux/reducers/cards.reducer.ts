f21cee8bd15683bd846ed894dc961762b3528550
Jjjulietta <jjjuliettai@gmail.com>
2023-11-20 14:01:33 +0300

    fix: change  reducers

--- youtube-client-app/src/app/redux/reducers/cards.reducer.ts
+++ youtube-client-app/src/app/redux/reducers/cards.reducer.ts
@@ -1,21 +1,66 @@
 import { createReducer, on } from '@ngrx/store';
+import { shareReplay } from 'rxjs';
 import { Favorite } from 'src/app/youtube/models/search-item.model';
 import { CardsApiActions } from '../actions/cards.actions';
-import { AppState } from '../state.models';
+import { AppState, YotubeResultsInterface } from '../state.models';
 
-export const initialState: Pick<AppState, 'youtubeCards'> = {
-  youtubeCards: [],
-};
+export const initialState: YotubeResultsInterface /*Pick<AppState, 'youtubeCards'>*/ =
+  {
+    isLoading: false,
+    cards: { '0': [] },
+    error: null,
+
+    //youtubeCards: {},
+  };
 
 export const cardsReducer = createReducer(
   initialState,
-  on(CardsApiActions.retrievedCardsList, (state, { cards }) => ({
+  on(CardsApiActions.getCards, (state, action) => ({
     ...state,
-    youtubeCards: [...cards],
+    isLoading: true,
   })),
-  on(CardsApiActions.addFavoritCard, (state, { cardId }) => ({
+  on(CardsApiActions.retrievedCardsList, (state, { cards, token }) => ({
     ...state,
-    youtubeCards: state.youtubeCards
+    isLoading: false,
+    cards: { ...state.cards, [token]: cards },
+  })),
+  on(CardsApiActions.getCardsError, (state, action) => ({
+    ...state,
+    isLoading: false,
+    error: action.error,
+  })),
+  on(CardsApiActions.addFavoritCard, (state, { cardId, token }) => ({
+    ...state,
+    cards: Object.fromEntries(
+      Object.entries(state.cards).map(([key, value]) => {
+        console.log(token);
+        if (key === token) {
+          console.log(key);
+          console.log(value);
+          return [
+            key,
+            value
+              .map((item) => ({ ...item }))
+              .map((item) => {
+                if (item.id === cardId) {
+                  console.log(item.id);
+                  return { ...item, favorite: Favorite.True };
+                } else {
+                  console.log(item.favorite);
+                  return item;
+                }
+              }),
+          ];
+          //console.log([key, value]);
+          //return [key, value];
+        } else {
+          console.log([key, value]);
+          return [key, value];
+        }
+      })
+    ),
+    //state.youtubeCards,
+    /*state.youtubeCards, ...state.youtubeCards[token]
       .map((item) => ({ ...item }))
       .map((item) => {
         if (item.id === cardId) {
@@ -23,11 +68,49 @@ export const cardsReducer = createReducer(
         } else {
           return item;
         }
-      }),
+      })*/
+
+    /*.map((item) => ({ ...item }))
+      .map((item) => {
+        if (item.id === cardId) {
+          return { ...item, favorite: Favorite.True };
+        } else {
+          return item;
+        }
+      }),*/
   })),
-  on(CardsApiActions.removeFavoritCard, (state, { cardId }) => ({
+  on(CardsApiActions.removeFavoritCard, (state, { cardId, token }) => ({
     ...state,
-    youtubeCards: state.youtubeCards
+    cards: Object.fromEntries(
+      Object.entries(state.cards).map(([key, value]) => {
+        console.log(token);
+        if (key === token) {
+          return [
+            key,
+            value
+              .map((item) => ({ ...item }))
+              .map((item) => {
+                if (item.id === cardId) {
+                  return { ...item, favorite: Favorite.False };
+                }
+                return item;
+              }),
+          ];
+        }
+        return [key, value];
+      })
+    ),
+    /*state.youtubeCards,
+    ...state.youtubeCards[token]
+      .map((item) => ({ ...item }))
+      .map((item) => {
+        if (item.id === cardId) {
+          return { ...item, favorite: Favorite.True };
+        } else {
+          return item;
+        }
+      }),*/
+    /*youtubeCards: state.youtubeCards
       .map((item) => ({ ...item }))
       .map((item) => {
         if (item.id === cardId) {
@@ -35,6 +118,6 @@ export const cardsReducer = createReducer(
         } else {
           return item;
         }
-      }),
+      }),*/
   }))
 );
