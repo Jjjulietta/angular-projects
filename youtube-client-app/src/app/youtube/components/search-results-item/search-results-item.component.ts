21aece58157f2ce44b0ab454d461ffe3c20ba8dd
Jjjulietta <jjjuliettai@gmail.com>
2023-11-20 14:20:49 +0300

    fix: add select page number to change favorite method

--- youtube-client-app/src/app/youtube/components/search-results-item/search-results-item.component.ts
+++ youtube-client-app/src/app/youtube/components/search-results-item/search-results-item.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { takeUntil } from 'rxjs';
+import { UnsubscribeService } from 'src/app/core/services/unsubscribe.service';
 import {
   CardsApiActions,
   CustomCardsActions,
 } from 'src/app/redux/actions/cards.actions';
+import { selectPageNumber } from 'src/app/redux/selectors/cards.selector';
 import {
   Favorite,
   SearchCards,
@@ -18,12 +21,18 @@ import {
 export class SearchResultsItemComponent {
   @Input() card!: SearchCards;
   @Input() cards!: SearchCards[];
+  pageNumber$ = this.store.select(selectPageNumber);
+  pageNumber!: string;
   favorite: Favorite = Favorite.False;
   custom: string = 'favorite video';
 
   date!: string | Date;
 
-  constructor(private router: Router, private store: Store) {}
+  constructor(
+    private router: Router,
+    private store: Store,
+    private unsubscribe$: UnsubscribeService
+  ) {}
   ngOnInit() {
     if (this.card?.date && this.card.date !== null) this.date = this.card?.date;
     if (this.card?.favorite) this.favorite = this.card?.favorite;
@@ -31,7 +40,10 @@ export class SearchResultsItemComponent {
 
   openCard() {
     console.log(this.card?.id);
-    this.router.navigate([`detailed/${this.card?.id}`]);
+    this.router.navigate([
+      `detailed/${this.card?.id}`,
+      { favorite: this.favorite },
+    ]);
   }
 
   removeCard(cardId: string) {
@@ -41,23 +53,35 @@ export class SearchResultsItemComponent {
   changeFavorite() {
     if (this.favorite === 'false') {
       this.favorite = Favorite.True;
-      this.card.id !== null
-        ? this.store.dispatch(
-            CardsApiActions.addFavoritCard({
-              cardId: this.card?.id,
-            })
-          )
-        : undefined;
+      //this.card.id !== null
+      this.store
+        .select(selectPageNumber)
+        .pipe(takeUntil(this.unsubscribe$))
+        .subscribe((val) => {
+          if (this.card.id !== null)
+            this.store.dispatch(
+              CardsApiActions.addFavoritCard({
+                cardId: this.card?.id,
+                token: val,
+              })
+            );
+        });
+
       this.custom = 'remove favorite';
     } else if (this.favorite === 'true') {
       this.favorite = Favorite.False;
-      this.card.id !== null
-        ? this.store.dispatch(
-            CardsApiActions.removeFavoritCard({
-              cardId: this.card?.id,
-            })
-          )
-        : undefined;
+      this.store
+        .select(selectPageNumber)
+        .pipe(takeUntil(this.unsubscribe$))
+        .subscribe((val) => {
+          if (this.card.id !== null)
+            this.store.dispatch(
+              CardsApiActions.removeFavoritCard({
+                cardId: this.card?.id,
+                token: val,
+              })
+            );
+        });
       this.custom = 'add favorite';
     }
   }
