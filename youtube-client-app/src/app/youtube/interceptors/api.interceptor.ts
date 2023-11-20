5b63729d67fedca7f8dc8c8a0f6ccff8e9831d0f
Jjjulietta <jjjuliettai@gmail.com>
2023-11-20 14:29:22 +0300

    refactor: change key

--- youtube-client-app/src/app/youtube/interceptors/api.interceptor.ts
+++ youtube-client-app/src/app/youtube/interceptors/api.interceptor.ts
@@ -10,7 +10,7 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
   private URL = 'https://www.googleapis.com/youtube/v3';
-  private API_KEY = 'AIzaSyB8zQEc9EzWukDP-CpmnKs6y93sA84B90Y';
+  private API_KEY = 'AIzaSyBPJb1EGYR949ZdWnL_SCBqXP0CUtZgQ2Y'; //'AIzaSyD7fJvlZR3KM_3rzX4n00zQazTk3RXUzBA'; //'AIzaSyC1F20A2M2pKe6HMjqS7TYMF9UqdGMMzGg'; //'AIzaSyB8zQEc9EzWukDP-CpmnKs6y93sA84B90Y';
   constructor() {}
 
   intercept(
