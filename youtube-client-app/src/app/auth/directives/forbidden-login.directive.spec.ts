14e0531864b37f8b2567b000529a02776078299c
Jjjulietta <jjjuliettai@gmail.com>
2023-11-26 20:15:35 +0300

    refactor: delete empty test files

--- youtube-client-app/src/app/auth/directives/forbidden-login.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ForbiddenLoginDirective } from './forbidden-login.directive';
-
-/*describe('ForbiddenLoginDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ForbiddenLoginDirective();
-    expect(directive).toBeTruthy();
-  });
-});*/
