d077ea5ba175e89b988e33d73e44052fa0ffbb64
Jjjulietta <jjjuliettai@gmail.com>
2023-12-01 20:34:43 +0300

    feat: add paths to app routing and update app module

--- connections-task/connections/src/app/app-routing.module.ts
+++ connections-task/connections/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'signin', pathMatch: 'full' },
   {
     path: 'signin',
+    pathMatch: 'full',
     loadComponent: () =>
       import('./login/login.component').then((m) => m.LoginComponent),
   },
@@ -15,6 +16,21 @@ const routes: Routes = [
       import('./registration/registration.component').then(
         (m) => m.RegistrationComponent
       ),
+  },
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  {
+    path: 'profile',
+    loadComponent: () =>
+      import('./profile/profile.component').then((m) => m.ProfileComponent),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'main',
+    loadComponent: () =>
+      import('./people-groups/people-groups.component').then(
+        (m) => m.PeopleGroupsComponent
+      ),
+    canActivate: [authGuard],
   },
 ];
 
--- connections-task/connections/src/app/app.module.ts
+++ connections-task/connections/src/app/app.module.ts
@@ -3,7 +3,7 @@ import {
   HttpClientModule,
   HTTP_INTERCEPTORS,
 } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +12,17 @@ import { ApiInterceptor } from './interceptors/api.interceptor';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { HttpService } from './services/http.service';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { ProfileComponent } from './profile/profile.component';
+import { userReducer } from './store/reducers/user.reduser';
+import { UserEffects } from './store/effects/user.effect';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { PeopleGroupsComponent } from './people-groups/people-groups.component';
+import { authGuard } from './guards/auth.guard';
+import { UnsubscribeService } from './services/unsubscribe.service';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { UpdateUserEffects } from './store/effects/update-user.effect';
 
 @NgModule({
   declarations: [AppComponent],
@@ -21,6 +32,11 @@ import { HttpService } from './services/http.service';
     HttpClientModule,
     RegistrationComponent,
     LoginComponent,
+    ProfileComponent,
+    PeopleGroupsComponent,
+    StoreModule.forRoot({ user: userReducer }, {}),
+    EffectsModule.forRoot([UserEffects, UpdateUserEffects]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
   ],
   providers: [
     {
@@ -28,7 +44,9 @@ import { HttpService } from './services/http.service';
       useClass: ApiInterceptor,
       multi: true,
     },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     { provide: HttpService },
+    { provide: UnsubscribeService },
   ],
   bootstrap: [AppComponent],
 })
