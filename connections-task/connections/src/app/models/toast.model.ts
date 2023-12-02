186bc643d245bdf9527331e6da73e16d63615286
Jjjulietta <jjjuliettai@gmail.com>
2023-12-02 15:33:06 +0300

    feat: create toast enums

---
+++ connections-task/connections/src/app/models/toast.model.ts
@@ -0,0 +1,11 @@
+export enum ToastMessage {
+  SucsessLogin = 'You have successfully logged in',
+  SucsessSignin = 'You have successfully registered!',
+  Error = 'error',
+  ErrorType = 'PrimaryDuplicationException',
+}
+
+export enum ToastState {
+  Sucsess = 'sucsess-toast',
+  Error = 'error-toast',
+}
