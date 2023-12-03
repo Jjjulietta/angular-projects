9504dfede195357143d0e0cfc61d0d21e8a3d451
Jjjulietta <jjjuliettai@gmail.com>
2023-12-03 09:24:25 +0300

    feat: add groups reducers

---
+++ connections-task/connections/src/app/store/reducers/groups.reducer.ts
@@ -0,0 +1,37 @@
+import { createReducer, on } from '@ngrx/store';
+import { GroupsActions } from '../actions/groups.action';
+import { GroupsData } from '../store.model';
+
+export const initialGroupsState: GroupsData = {
+  isLoading: false,
+  groups: null,
+  error: null,
+};
+
+export const groupReducer = createReducer(
+  initialGroupsState,
+  on(GroupsActions.createGroup, (state, { name }) => ({
+    ...state,
+    isLoading: true,
+  })),
+  on(GroupsActions.getGroups, (state) => ({ ...state, isLoading: true })),
+  on(GroupsActions.addGroup, (state, { group }) => ({
+    ...state,
+    isLoading: false,
+    groups: state.groups,
+    group,
+  })),
+  on(GroupsActions.retrievedGroups, (state, { groups }) => ({
+    ...state,
+    isLoading: false,
+    groups: groups,
+  })),
+  on(GroupsActions.getGroupsError, (state, { error }) => ({
+    ...state,
+    error: error,
+  })),
+  on(GroupsActions.deleteGroup, (state, { groupId }) => ({
+    ...state,
+    ...state.groups?.filter((item) => item.id !== groupId),
+  }))
+);
