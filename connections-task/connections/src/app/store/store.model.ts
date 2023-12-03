eec397299732d3e321b5a4956d34b707a1f13cf4
Jjjulietta <jjjuliettai@gmail.com>
2023-12-03 09:22:35 +0300

    feat: create interfaces for groups

--- connections-task/connections/src/app/store/store.model.ts
+++ connections-task/connections/src/app/store/store.model.ts
@@ -1,11 +1,19 @@
+import { Group } from '../models/group.model';
 import { AuthUser, User } from '../models/login.model.ts';
 
 export interface AppState {
   user: UserData;
+  groups: GroupsData;
 }
 
 export interface UserData {
   isLoading: boolean;
   authUser: User | null;
+  error: string | null;
+}
+
+export interface GroupsData {
+  isLoading: boolean;
+  groups: Group[] | null;
   error: string | null;
 }
