0c8b7361ca18ff7fbcba47ae7a5d1e3a78701ab9
Jjjulietta <jjjuliettai@gmail.com>
2023-12-03 09:32:44 +0300

    feat: implement a list of groups into the people groups component

--- connections-task/connections/src/app/people-groups/people-groups.component.ts
+++ connections-task/connections/src/app/people-groups/people-groups.component.ts
@@ -1,18 +1,72 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { Group } from '../models/group.model';
+import { Store } from '@ngrx/store';
+import { selectGroups } from '../store/selectors/groups.selectors';
+import { GroupsActions } from '../store/actions/groups.action';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { HttpService } from '../services/http.service';
+import { takeUntil } from 'rxjs';
+import { UnsubscribeService } from '../services/unsubscribe.service';
 
 @Component({
   selector: 'app-people-groups',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './people-groups.component.html',
   styleUrls: ['./people-groups.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PeopleGroupsComponent {
-  constructor(private router: Router) {}
+  groups$ = this.store.select(selectGroups);
+  groups!: Group[];
+  isShown: boolean = false;
+  popapForm: FormGroup = new FormGroup({
+    nameGroup: new FormControl('', { nonNullable: true }),
+  });
+  constructor(
+    private router: Router,
+    private store: Store,
+    private service: HttpService,
+    private unsubscribe$: UnsubscribeService
+  ) {
+    this.store.dispatch(GroupsActions.getGroups());
+  }
+
+  ngOnInit() {
+    this.popapForm = new FormGroup({
+      nameGroup: new FormControl('', [
+        Validators.maxLength(30),
+        //Validators.pattern('/'),
+      ]),
+    });
+  }
+
+  get nameGroup() {
+    return this.popapForm.get('nameGroup');
+  }
 
   openProfile() {
     this.router.navigate(['profile']);
+  }
+
+  updateGroups() {}
+
+  createGroup() {
+    this.isShown = true;
+  }
+
+  createGroupSubmit() {
+    const name = this.popapForm.value.nameGroup;
+    console.log(name);
+    this.store.dispatch(GroupsActions.createGroup(name));
+    this.groups$ = this.store.select(selectGroups);
   }
 }
