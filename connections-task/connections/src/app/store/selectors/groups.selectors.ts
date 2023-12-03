705744a7ba8127eaaccd4c9083d79e951afaf3b9
Jjjulietta <jjjuliettai@gmail.com>
2023-12-03 09:25:00 +0300

    feat: add groups selectors

---
+++ connections-task/connections/src/app/store/selectors/groups.selectors.ts
@@ -0,0 +1,6 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { GroupsData } from '../store.model';
+
+export const selectGroupsData = createFeatureSelector<GroupsData>('groups');
+
+export const selectGroups = createSelector(selectGroupsData, (s1) => s1.groups);
