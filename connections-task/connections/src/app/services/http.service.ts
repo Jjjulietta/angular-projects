293d086233e1988d23c3b6821a63d556b5ce6fd3
Jjjulietta <jjjuliettai@gmail.com>
2023-12-04 12:55:04 +0300

    feat: add http requests for groups and conversations list

--- connections-task/connections/src/app/services/http.service.ts
+++ connections-task/connections/src/app/services/http.service.ts
@@ -2,9 +2,11 @@ import {
   HttpClient,
   HttpErrorResponse,
   HttpHeaders,
+  HttpParams,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, map, tap, throwError } from 'rxjs';
+import { Conversation, ConversationsModel } from '../models/conversation.model';
 import { Group, GroupsModel } from '../models/group.model';
 import {
   AuthUser,
@@ -13,6 +15,10 @@ import {
   User,
   UserProfile,
 } from '../models/login.model.ts';
+import { People, PeopleModel, UserModel } from '../models/people.model';
+import { ToastState } from '../models/toast.model';
+import { PeopleData, UserData } from '../store/store.model';
+import { ToastService } from './toast.service';
 
 @Injectable({
   providedIn: 'root',
@@ -23,8 +29,11 @@ export class HttpService {
   private PROFILE = 'profile';
   private GROUPS_LIST = 'groups/list';
   private GROUPS_CREATE = 'groups/create';
+  private GROUPS_DELETE = 'groups/delete';
+  private USERS = 'users';
+  private CONVERSATIONS_LIST = 'conversations/list';
   //private URL = 'https://tasks.app.rs.school/angular/registration';
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient, private toast: ToastService) {}
 
   signup(body: Registration) {
     const httpOptions = {
@@ -55,13 +64,6 @@ export class HttpService {
   }
 
   getUser(user: AuthUser) {
-    /*const httpOptions = {
-      headers: new HttpHeaders({
-        'rs-uid': `${user.uid}`,
-        'rs-email': `${user.email}`,
-        Authorization: `Bearer ${user.token}`,
-      }),
-    };*/
     return this.httpClient.get<UserProfile>(this.PROFILE).pipe(
       map((r) => {
         const obj: User = {
@@ -112,14 +114,54 @@ export class HttpService {
 
   createGroup(nameGroup: string) {
     return this.httpClient
-      .post<Pick<Group, 'id'>>(this.GROUPS_CREATE, { name: { nameGroup } })
+      .post<{ groupID: string }>(this.GROUPS_CREATE, { name: nameGroup })
       .pipe(
         map((r) => r),
         catchError(this.handleError)
       );
   }
 
-  private handleError(error: HttpErrorResponse) {
+  deleteGroup(groupId: string) {
+    const params = new HttpParams().set('groupID', `${groupId}`);
+    return this.httpClient
+      .delete(this.GROUPS_DELETE, { observe: 'response', params: params })
+      .pipe(
+        tap((val) => console.log(val)),
+        catchError(this.handleError)
+      );
+  }
+
+  getPeople() {
+    return this.httpClient.get<People>(this.USERS).pipe(
+      map((r) => {
+        console.log(r);
+        return r.Items.map((item) => {
+          const obj: UserModel = { name: item.name.S, id: item.uid.S };
+          return obj;
+        });
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  getConversations() {
+    return this.httpClient
+      .get<ConversationsModel>(this.CONVERSATIONS_LIST)
+      .pipe(
+        map((r) => {
+          return r.Items.map((item) => {
+            const obj: Conversation = {
+              id: item.id.S,
+              companionID: item.companionID.S,
+            };
+            return obj;
+          });
+        }),
+        catchError(this.handleError)
+      );
+  }
+
+  handleError(error: HttpErrorResponse) {
     if (error.status === 0) {
       console.log('An error occurred:', error.error);
     } else {
