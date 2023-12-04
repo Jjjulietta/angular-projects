85b165dc34e99fb7011427a80f495fd773e7371b
Jjjulietta <jjjuliettai@gmail.com>
2023-12-04 12:58:33 +0300

    feat: create people selectors

---
+++ connections-task/connections/src/app/store/selectors/people.selectors.ts
@@ -0,0 +1,5 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { AppState, PeopleData } from '../store.model';
+
+export const selectPeopleData = createFeatureSelector<PeopleData>('people');
+export const selectPeople = createSelector(selectPeopleData, (s1) => s1.people);
