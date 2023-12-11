851d98766c1f7d304fdd6cc68be2b551dfb35108
Jjjulietta <jjjuliettai@gmail.com>
2023-12-11 16:26:40 +0300

    feat: add select message error to messages selectors

--- connections-task/connections/src/app/store/selectors/messages.selectors.ts
+++ connections-task/connections/src/app/store/selectors/messages.selectors.ts
@@ -8,3 +8,8 @@ export const selectMessages = createSelector(
   selectMessagesData,
   (s1) => s1.messages
 );
+
+export const selectMessageError = createSelector(
+  selectMessagesData,
+  (s1) => s1.error
+);
