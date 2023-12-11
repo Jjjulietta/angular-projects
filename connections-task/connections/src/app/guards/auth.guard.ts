534b8b4675451666f704ef3e2d3908787acbeea9
Jjjulietta <jjjuliettai@gmail.com>
2023-12-11 17:11:32 +0300

    fix: implement auth service to guard

--- connections-task/connections/src/app/guards/auth.guard.ts
+++ connections-task/connections/src/app/guards/auth.guard.ts
@@ -1,10 +1,12 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  if (localStorage.getItem('authUser')) {
+  if (inject(AuthService).checkAuth()) {
     return true;
+  } else {
+    inject(Router).navigate(['signin']);
+    return false;
   }
-  inject(Router).navigate(['signin']);
-  return false;
 };
