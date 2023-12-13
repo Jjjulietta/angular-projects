2ff2b18d4786b818d950fcff3d6d9aef4312ba0d
Jjjulietta <jjjuliettai@gmail.com>
2023-12-13 20:42:26 +0300

    feat: add email validator

--- connections-task/connections/src/app/registration/registration.component.ts
+++ connections-task/connections/src/app/registration/registration.component.ts
@@ -6,21 +6,25 @@ import {
   FormBuilder,
   Validators,
   ReactiveFormsModule,
+  ValidatorFn,
+  AbstractControl,
+  ValidationErrors,
 } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { forbiddenValidator } from '../directive/forbidden-password.directive';
 import { Registration } from '../models/login.model.ts';
 import { HttpService } from '../services/http.service';
 import { ToastService } from '../services/toast.service';
 import { takeUntil } from 'rxjs';
 import { UnsubscribeService } from '../services/unsubscribe.service';
-import { ToastMessage, ToastState } from '../models/toast.model';
+import { ErrorMessages, ToastMessage, ToastState } from '../models/toast.model';
 import { HttpErrorResponse } from '@angular/common/http';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss'],
 })
@@ -36,27 +40,37 @@ export class RegistrationComponent {
   type: string = 'password';
   icon = '../../../../assets/visibility_off_FILL0_wght200_GRAD0_opsz20.svg';
   user: Registration | undefined;
+  emailsDuplicate: string[] = [];
 
   constructor(
     private fb: FormBuilder,
     private router: Router,
     private httpService: HttpService,
     private unsubscribe$: UnsubscribeService,
-    private toastService: ToastService
+    private toastService: ToastService,
+    private authService: AuthService
   ) {
+    //let emailsDuplikate: string[] | [] = [];
     /*this.authService.user.subscribe((val: Auth) => {
       this.user = val;
     });*/
   }
 
   ngOnInit() {
+    if (localStorage.getItem('emails')) {
+      localStorage.removeItem('emails');
+    }
     this.regForm = new FormGroup({
       name: new FormControl('', [
         Validators.required,
         Validators.pattern(/[\p{L}\s]+$/gu),
         Validators.maxLength(40),
       ]),
-      email: new FormControl('', [Validators.required, Validators.email]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email,
+        this.emailValidator(),
+      ]),
       password: new FormControl('', [
         Validators.required,
         forbiddenValidator(),
@@ -101,14 +115,23 @@ export class RegistrationComponent {
               this.router.navigate(['signin']);
             }, 6000);
           },
-          error: (error: { type: string; message: string }) => {
-            console.log(error.message);
+          error: (error: ErrorMessages) => {
             this.toastService.showToast(error.message, ToastState.Error);
-            if (error.message === ToastMessage.ErrorType) {
-              console.log(error.message);
+            if (error.type === ToastMessage.ErrorType) {
+              if (this.regForm.value.email) {
+                let email: string = this.regForm.value.email;
+                //let emailsDuplikate = []
+                this.emailsDuplicate.push(email);
+                //this.authService.emails$ = this.emailsDuplicate;
+                localStorage.setItem(
+                  'emails',
+                  JSON.stringify(this.emailsDuplicate)
+                );
+              }
+              console.log(error.type);
               this.errorType = error.message;
             }
-            this.regForm.invalid;
+            //this.regForm.invalid;
           },
         });
     }
@@ -128,5 +151,46 @@ export class RegistrationComponent {
       this.icon =
         '../../../../assets/visibility_FILL0_wght200_GRAD0_opsz20.svg';
     }
+  }
+
+  emailValidator(): ValidatorFn {
+    return (control: AbstractControl<string>): ValidationErrors | null => {
+      this.errorType = undefined;
+      console.log(control.value);
+      console.log(this.regForm.valid);
+      /*this.authService
+        .getEmailsDuplicate$()
+        .pipe(takeUntil(this.unsubscribe$))
+        .subscribe((val) => {
+          console.log(val);
+          this.emailsDuplicate = val;
+          const emailValid = control.value
+            ? !this.emailsDuplicate.includes(control.value)
+            : false;
+          if (!emailValid) {
+            return { forbiddenEmail: 'User {email} already exists' };
+          }
+          return null;
+        });
+      return null;*/
+
+      if (localStorage.getItem('emails')) {
+        const emailsStr = localStorage.getItem('emails');
+        if (emailsStr) {
+          const emails: string[] = JSON.parse(emailsStr);
+          const emailValid = control.value
+            ? !emails.includes(control.value)
+            : false;
+          console.log(this.regForm.valid);
+          console.log(emailValid);
+          if (!emailValid) {
+            return { forbiddenEmail: 'User {email} already exists' };
+          }
+        }
+      }
+      console.log(this.regForm.valid);
+      this.disabled = false;
+      return null;
+    };
   }
 }
