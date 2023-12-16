e189de1fe079de7eb37d4eab4c41186b077e4ee0
Jjjulietta <jjjuliettai@gmail.com>
2023-12-16 21:07:35 +0300

    feat: add error selector to user

--- connections-task/connections/src/app/store/selectors/user.selectors.ts
+++ connections-task/connections/src/app/store/selectors/user.selectors.ts
@@ -6,3 +6,4 @@ export const selectUser = createFeatureSelector<UserData>('user');
 
 export const selectUserData = createSelector(selectUser, (s1) => s1.authUser);
 export const selectUserId = createSelector(selectUser, (s1) => s1.authUser?.id);
+export const selectUserError = createSelector(selectUser, (s1) => s1.error);
