796f12c0684068adcfbb39dddca1dd55f8493d1f
Jjjulietta <jjjuliettai@gmail.com>
2023-12-16 17:34:09 +0300

    feat: add toast enum

--- connections-task/connections/src/app/models/toast.model.ts
+++ connections-task/connections/src/app/models/toast.model.ts
@@ -1,16 +1,26 @@
 export enum ToastMessage {
-  SucsessLogin = 'You have successfully logged in',
-  SucsessSignin = 'You have successfully registered!',
-  SucsessDelete = 'Group successfully deleted',
-  SiccessCreate = 'Group successfulley created',
-  SucsessDeleteConv = 'Conversation successfully deleted',
-  SiccessCreateConv = 'Conversation successfulley created',
-  Error = 'error',
+  SucsessLogin = 'You have sucsessfully logged in',
+  SucsessSignin = 'You have sucsessfully registered!',
+  SucsessDelete = 'Group sucsessfully deleted',
+  SucsessUpdateGroup = 'Group list sucsessfully updated',
+  SucsessUpdatePeople = 'User list sucsessfully updated',
+  SucsessUpdateMessages = 'Message list sucsessfully updated',
+  SucsessCreate = 'Group sucsessfully created',
+  SucsessDeleteConv = 'Conversation sucsessfully deleted',
+  SucsessCreateConv = 'Conversation sucsessfully created',
+  SucsessUpdateUserName = 'User name sucsessfully updated',
+  Error = 'An error occurred',
 
   ErrorType = 'PrimaryDuplicationException',
+  ErrorTypeNotFound = 'NotFoundException',
 }
 
 export enum ToastState {
   Sucsess = 'sucsess-toast',
   Error = 'error-toast',
+}
+
+export interface ErrorMessages {
+  type: string;
+  message: string;
 }
