e895438d70b48e880d4ec7426b63af5669aafb6d
Jjjulietta <jjjuliettai@gmail.com>
2023-12-16 17:42:54 +0300

    feat: add reactive form and validation to profile component

--- connections-task/connections/src/app/profile/profile.component.ts
+++ connections-task/connections/src/app/profile/profile.component.ts
@@ -4,21 +4,24 @@ import {
   Component,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, NgModel } from '@angular/forms';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthUser, User } from '../models/login.model.ts';
+import { AuthUser } from '../models/login.model.ts';
 import { Store } from '@ngrx/store';
-import { selectUser, selectUserData } from '../store/selectors/user.selectors';
+import { selectUserData } from '../store/selectors/user.selectors';
 import { UserActions } from '../store/actions/user.actions';
-import { Options } from '../models/options.model';
 import { UnsubscribeService } from '../services/unsubscribe.service';
 import { takeUntil } from 'rxjs';
-import { TimerService } from '../services/timer.service';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -30,23 +33,39 @@ export class ProfileComponent {
   nextName: string = '';
   userEmail: string | null = '';
   date: Date | null = null;
-  dateLong: string = '';
   isShown: boolean = false;
   button: string = 'edit';
   user!: AuthUser;
+  disable: boolean = false;
+  formName = this.fb.group({ name: [''] });
 
   constructor(
     private router: Router,
     private store: Store,
     private cd: ChangeDetectorRef,
-    private unsubscribe$: UnsubscribeService
-  ) {
-    //this.userData();
-  }
+    private unsubscribe$: UnsubscribeService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit() {
     this.userData();
+
+    this.formName = this.fb.group({
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^[\p{L}\s]*$/u),
+          Validators.maxLength(40),
+        ],
+      ],
+    });
   }
+
+  get name() {
+    return this.formName.get('name');
+  }
+
   getUser() {
     const str = localStorage.getItem('authUser');
     if (str !== null) {
@@ -64,9 +83,6 @@ export class ProfileComponent {
         this.userEmail = val.email;
         this.userName = val.name;
         this.date = val.date;
-        /*console.log(this.userName);
-        console.log(this.date?.getFullYear());
-        this.date !== null ? this.getDate(this.date) : '';*/
       } else {
         this.getUser();
       }
@@ -75,39 +91,24 @@ export class ProfileComponent {
 
   onClick() {
     if (this.isShown) {
-      this.userName = this.nextName;
-      this.store.dispatch(UserActions.updateUser({ name: this.userName }));
-      this.button = 'edit';
-      this.isShown = false;
+      if (this.formName.value.name) {
+        this.nextName = this.formName.value.name;
+        this.userName = this.nextName;
+        this.store.dispatch(UserActions.addUser({ name: this.userName }));
+        this.button = 'edit';
+        this.isShown = false;
+      }
     } else {
       this.button = 'save';
       this.isShown = true;
     }
   }
 
-  /*getDate(date: Date) {
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = date.getDate();
-    const weekday = date.getDay();
-    let options: Options = {
-      weekday: 'long',
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-    };
-
-    let dateNew = new Date(year, month, day, weekday);
-    let str: string = dateNew.toLocaleDateString('en-US', options);
-    this.dateLong = str;
-  }*/
-
   backMain() {
     this.router.navigate(['']);
   }
 
   reset() {
-    //this.userName = this.previousName;
     this.isShown = false;
   }
 
