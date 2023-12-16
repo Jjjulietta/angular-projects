1d6c6952804b3271107551d778fe9c39a28844b7
Jjjulietta <jjjuliettai@gmail.com>
2023-12-16 17:35:56 +0300

    feat: add select error to people selectors

--- connections-task/connections/src/app/store/selectors/people.selectors.ts
+++ connections-task/connections/src/app/store/selectors/people.selectors.ts
@@ -4,3 +4,7 @@ import { AppState, PeopleData } from '../store.model';
 export const selectPeopleData = createFeatureSelector<PeopleData>('people');
 export const selectPeople = createSelector(selectPeopleData, (s1) => s1.people);
 export const selectUsers = createSelector(selectPeopleData, (s1) => s1.names);
+export const selectUsersError = createSelector(
+  selectPeopleData,
+  (s1) => s1.error
+);
